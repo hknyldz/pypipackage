f2055a721a17104866517fcc7752a8acb96165ef
talhatarik <talhatarik@talhatarik-MacBook-Pro.local>
2019-06-04 02:39:34 +0300

    #pypi-package

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'paketAdi',
+    packages = ['.'],
+    version = '0.1.0',
+    description = 'Buraya açıklama',
+    author = 'Uygulama Sahibi Adı',
+    author_email = 'emailadresi@email.com',
+    url = 'githubURL',
+    keywords = ['TAG1', 'TAG2'],
+    classifiers = [],
+)
